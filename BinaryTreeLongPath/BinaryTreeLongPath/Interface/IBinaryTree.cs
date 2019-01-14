33cb409313aaa5e789567e8b1b9011bbe8b293c4
schulzy <schulzyy@gmail.com>
2019-01-14 18:38:21 +0100

    Update 3

---
+++ BinaryTreeLongPath/BinaryTreeLongPath/Interface/IBinaryTree.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace BinaryTreeLongPath.Interface
+{
+    public interface IBinaryTree
+    {
+        LinkedList<LinkedList<int>> Tree { get; }
+        void AddLineBottom(LinkedList<int> line);
+        void AddLineTop(LinkedList<int> line);
+    }
+}
